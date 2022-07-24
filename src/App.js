df85562c3e82724604544dc8240233d4ff17964e
Osama ayub <77015981+osamaayub@users.noreply.github.com>
2022-07-24 14:34:49 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import logo from './logo.svg';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div >
+  );
+}
+
+export default App;
